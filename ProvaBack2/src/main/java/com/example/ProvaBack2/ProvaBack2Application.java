16b196ada24a9736561a50297bca9dfff29dea91
GabrielPiscke <gabrielmateuspiscke@gmail.com>
2025-04-23 13:45:12 -0300

    Upd1

---
+++ ProvaBack2/src/main/java/com/example/ProvaBack2/ProvaBack2Application.java
@@ -0,0 +1,13 @@
+package com.example.ProvaBack2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaBack2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaBack2Application.class, args);
+	}
+
+}
